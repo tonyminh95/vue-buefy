48cc8a7073bad36849fa410e862f183e0d9f516a
tonyminh95 <minhbui12121995@gmail.com>
2019-07-07 17:31:11 +0900

    setup sass

---
+++ src/layout/DashboardLayout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <!-- <side-bar></side-bar>
+        <nav-bar></nav-bar> -->
+        <Footer/>
+    </div>
+</template>
+
+<script>
+    import SideBar from './TheSideBar'
+    import NavBar from './TheNavBar'
+    import Footer from './TheFooter'
+
+    export default {
+        components: {
+            SideBar,
+            NavBar,
+            Footer
+        }
+    }
+</script>
---
+++ src/layout/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+                Created by <strong>TonyMinh</strong>
+                <b-icon icon="account" size="is-small"></b-icon>
+            </p>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
